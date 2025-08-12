e2af23b50312f9f8b94abea0dc4ddfb9fcee7c0d
Vikas Kumar <vkviki0786@gmail.com>
2025-08-12 09:22:51 +0530

    app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = '@gmail.com'  # Your Gmail address
-app.config['MAIL_PASSWORD'] =  # User's Gmail app password (no spaces)
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = '@gmail.com'
 
 # Set timezone
